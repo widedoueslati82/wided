765719c8919ace748398e35772eab266aab08873
widedoueslati82 <167637733+widedoueslati82@users.noreply.github.com>
2025-11-20 11:17:40 -0500

    3 eme commit pour modifier le fichier la branche 1
    3commit pour modifier le fichier la branche online

--- hello/hello/obj/Debug/net8.0/hello.AssemblyInfo.cs
+++ hello/hello/obj/Debug/net8.0/hello.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+881f263689d2b5cd709cce858b07ed5811485ff1")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
